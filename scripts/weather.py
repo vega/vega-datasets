7dd09019a698cbec702fb0a35546c16060693c7e
Visnu Pitiyanuvath <visnup@gmail.com>
2020-02-04 10:22:43 -0800

    Make corresponding changes to the weather script
    Removed thunder as an indicator

--- scripts/weather.py
+++ scripts/weather.py
@@ -6,25 +6,24 @@ reader = csv.reader(sys.stdin)
 headers = reader.next()
 
 
-# WT14 - Drizzle
-# WT03 - Thunder
+# WT01 - Fog, ice fog, or freezing fog (may include heavy fog)
+# WT02 - Heavy fog or heaving freezing fog (not always distinguished from fog)
 # WT04 - Ice pellets, sleet, snow pellets, or small hail"
 # WT05 - Hail (may include small hail)
+# WT08 - Smoke or haze
+# WT13 - Mist
+# WT14 - Drizzle
 # WT16 - Rain (may include freezing rain, drizzle, and freezing drizzle)"
 # WT17 - Freezing rain
 # WT18 - Snow, snow pellets, snow grains, or ice crystals
-# WT08 - Smoke or haze
-# WT11 - High or damaging winds
 # WT22 - Ice fog or freezing fog
-# WT01 - Fog, ice fog, or freezing fog (may include heavy fog)
-# WT02 - Heavy fog or heaving freezing fog (not always distinguished from fog)
-# WT13 - Mist
 
 # AWND - Average daily wind speed (tenths of meters per second)
 # TOBS - Temperature at the time of observation (tenths of degrees C)
 # TMAX - Maximum temperature (tenths of degrees C)
 # TMIN - Minimum temperature (tenths of degrees C)
 # PRCP - Precipitation (tenths of mm)
+# SNOW - Snowfall (mm)
 
 names = ['DATE', 'PRCP', 'TMAX', 'TMIN', 'AWND', 'WEATHER']
 writer = csv.DictWriter(sys.stdout, fieldnames=names, extrasaction='ignore')
@@ -43,10 +42,10 @@ for row in reader:
     if d['WT14'] == '1' or d['WT08'] == '1' or d['WT13'] == '1':
         d['WEATHER'] = 'drizzle'
 
-    if d['WT16'] == '1' or d['WT17'] == '1' or d['WT05'] == '1' or d['WT03'] == '1':
+    if d['WT16'] == '1' or d['WT17'] == '1' or d['WT05'] == '1' or int(d['PRCP']) > 0:
         d['WEATHER'] = 'rain'
 
-    if d['WT18'] == '1' or d['WT04'] == '1':
+    if d['WT18'] == '1' or d['WT04'] == '1' or int(d['SNOW']) > 0:
         d['WEATHER'] = 'snow'
 
     date = d['DATE']
