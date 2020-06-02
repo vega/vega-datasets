08f6e6c3717061a39de538b330197f6d8fe82c58
Dominik Moritz <domoritz@gmail.com>
2020-06-02 09:55:04 -0700

    feat: load vega datasets from jsdelivr
    fixes #189

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
-  input: "build/index.js",
+  input: "build/src/index.js",
   output: {
     file: "build/vega-datasets.js",
     format: "umd",
     sourcemap: true,
     name: "vegaDatasets"
   },
-  plugins: [ resolve() ]
+  plugins: [resolve(), json()]
 };
