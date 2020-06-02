fb87bde22bb640de78f6bb40cbe8490c333ed8fc
Dominik Moritz <domoritz@gmail.com>
2020-06-02 09:55:04 -0700

    feat: load vega datasets from jsdelivr
    fixes #189

--- scripts/make-url-index.sh
+++ scripts/make-url-index.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "import version from '../package.json';"
 echo 'export default {'
+echo ''
 
 for file in data/*.json data/*.csv data/*.tsv data/*.arrow; do
     name=${file##*/}
-    echo "  '$name': 'https://vega.github.io/vega-datasets/data/$name',"
+    echo "  '$name': \`https://cdn.jsdelivr.net/npm/vega-datasets@\${version}/data/$name\`,"
 done
 
 echo '}'
