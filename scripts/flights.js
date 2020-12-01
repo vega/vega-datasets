0d982e8c83ce4ac0425c94232171cb6219371049
P42 <p42@p42.ai>
2020-12-01 17:38:08 +0000

    Use Blocked Scoped Variables
    **'Let' and 'const' are block-scope variable declarations that can replace 'var' declarations in many cases.**
    
    `const` declares blocked-scoped variables that cannot be re-assigned. `let` declares block-scoped variables that can be changed. They are available since ES6 and are preferred over function-scope, modifiable `var` declarations, because they make it easier to reason about the code.
    
    When there are several variables declared in a statement, this refactoring does not change the declaration to prevent formatting breakages and linter issues.
    
    `var` declarations are only changed if they are used within block-scope, and only variables that are not modified are marked as `const`.

--- scripts/flights.js
+++ scripts/flights.js
@@ -1,8 +1,8 @@
-var fs = require('fs'),
-    dl = require('datalib');
+const fs = require('fs'),
+      dl = require('datalib');
 
 function getRandomSubarray(arr, size) {
-  var shuffled = arr.slice(0), i = arr.length, temp, index;
+  let shuffled = arr.slice(0), i = arr.length, temp, index;
   while (i--) {
     index = Math.floor((i + 1) * Math.random());
     temp = shuffled[index];
@@ -16,14 +16,14 @@ function formatDate(d) {
   return "2001/"+d.substring(0, 2)+"/"+d.substring(2, 4)+" "+d.substring(4, 6)+":"+d.substring(6, 8);
 }
 
-var flights = dl.csv('data/flights-3m.csv', {
+const flights = dl.csv('data/flights-3m.csv', {
     parse: {date: 'string', 'delay': 'number', 'distance': 'number'}
 });
 
-var N = +process.argv[2] || 2000,
-    fmt = dl.format.auto.number('s');
+const N = +process.argv[2] || 2000,
+      fmt = dl.format.auto.number('s');
 
-var randFlights = getRandomSubarray(flights, N).map(function(d) {
+const randFlights = getRandomSubarray(flights, N).map(function(d) {
   return (d.date = formatDate(d.date), d);
 });
 
