b183e3b60ea195b72abf6b4ac13cac21499d7a32
Dominik Moritz <domoritz@gmail.com>
2022-09-29 17:41:05 -0400

    chore: refactor builds and include sourcemaps and ts support

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,52 @@
-import resolve from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
+import json from "@rollup/plugin-json";
+import ts from "rollup-plugin-ts";
+import bundleSize from "rollup-plugin-bundle-size";
+import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "build/src/index.js",
-  output: {
-    file: "build/vega-datasets.js",
-    format: "umd",
-    sourcemap: true,
-    name: "vegaDatasets"
+const plugins = (browserslist, declaration) => [
+  json(),
+  ts({
+    tsconfig: (resolvedConfig) => ({
+      ...resolvedConfig,
+      declaration,
+      declarationMap: declaration,
+    }),
+    transpiler: "babel",
+    babelConfig: { presets: ["@babel/preset-env"] },
+    browserslist,
+  }),
+  bundleSize(),
+];
+
+const outputs = [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "build/vega-datasets.module.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins: plugins(undefined, true)
   },
-  plugins: [resolve(), json()]
-};
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "build/vega-datasets.js",
+        format: "umd",
+        sourcemap: true,
+        name: "vegaDatasets",
+      },
+      {
+        file: "build/vega-datasets.min.js",
+        format: "iife",
+        sourcemap: true,
+        name: "vegaDatasets",
+        plugins: [terser()],
+      },
+    ],
+    plugins: plugins("defaults and not IE 11", false)
+  },
+];
+
+export default outputs;
